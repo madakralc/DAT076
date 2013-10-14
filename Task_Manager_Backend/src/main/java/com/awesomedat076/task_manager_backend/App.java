6825d9ccfe38838ebccbfa5e1c7ea40d54327390
Adam <Adam@XENIT-AC>
2013-10-14 17:05:32 +0200

    Created the two projects

---
+++ Task_Manager_Backend/src/main/java/com/awesomedat076/task_manager_backend/App.java
@@ -0,0 +1,13 @@
+package com.awesomedat076.task_manager_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
