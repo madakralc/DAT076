c036042229f183815263c1f5e5ada98ccaf7a4c1
Adam <Adam@XENIT-AC>
2013-10-19 19:32:39 +0200

    Ändrat i flertalet filer som har med layout och design att göra

---
+++ Task_Manager_Frontend/src/main/webapp/js/time.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var currentdate = new Date();
+var datetime = "Last Sync: " + currentdate.getDay() + "/"+currentdate.getMonth() 
++ "/" + currentdate.getFullYear() + " @ " 
++ currentdate.getHours() + ":" 
++ currentdate.getMinutes() + ":" + currentdate.getSeconds();
