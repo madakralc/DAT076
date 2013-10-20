87155585e9b6f5b11d8f51a9ba9d6103435bb8ed
dagf <dagf@t431s.corp.daysoft.se>
2013-10-20 12:09:56 +0200

    Class for Items added.

---
+++ Task_Manager_Backend/src/main/java/com/awesomedat076/task_manager_backend/Item.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.awesomedat076.task_manager_backend;
+
+/**
+ * This is a class represents  an item in the the item list stored in the database. 
+ * @author dagf
+ */
+public class Item {
+    
+    private String name;
+    public Item(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+}
