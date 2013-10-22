62638f3d4415e9f80de8800065b51395ee886dbf
Adam <Adam@XENIT-AC>
2013-10-22 03:02:16 +0200

    Lagt in lite roliga features

--- Task_Manager_Frontend/src/main/webapp/js/listtable.js
+++ Task_Manager_Frontend/src/main/webapp/js/listtable.js
@@ -26,12 +26,12 @@ $(function() {
             $('#products').empty();
             
             tr = $('<tr class="ui-widget-header" />');
-            tr.append("<font size='60'><th font-size=20px><u>Name</u></th></font><br /><br />");
+            tr.append("<font size='60'><th font-size=20px><u>Todo List</u></th></font><br /><br />");
             $('#products').append(tr);
             
             for (var i = 0; i < data.length; i++) {
                 tr = $('<tr id="tr' + data[i].id + '" />');
-                tr.append("<td>" + data[i].name + "</td>");
+                tr.append("<td>" + "<button class='menubutton'>" + data[i].name + "</button>" + "</td>");
                 $('#products').append(tr);
             }
             
